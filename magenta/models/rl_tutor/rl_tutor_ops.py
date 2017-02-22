1679f149c77b8ca07423489af465ff606045bca5
Natasha Jaques <jaquesn@mit.edu>
2017-02-22 15:10:09 -0500

    it's actually contrib.rnn.rnn_cell?

--- magenta/models/rl_tutor/rl_tutor_ops.py
+++ magenta/models/rl_tutor/rl_tutor_ops.py
@@ -346,16 +346,16 @@ def make_cell(hparams, note_rnn_type, state_is_tuple=False):
   cells = []
   for num_units in hparams.rnn_layer_sizes:
     if note_rnn_type == 'default':
-      cell = tf.contrib.rnn_cell.LSTMCell(
+      cell = tf.contrib.rnn.rnn_cell.LSTMCell(
           num_units, state_is_tuple=state_is_tuple)
     else:
-      cell = tf.contrib.rnn_cell.BasicLSTMCell(
+      cell = tf.contrib.rnn.rnn_cell.BasicLSTMCell(
           num_units, state_is_tuple=state_is_tuple)
-      cell = tf.contrib.rnn_cell.DropoutWrapper(
+      cell = tf.contrib.rnn.rnn_cell.DropoutWrapper(
           cell, output_keep_prob=hparams.dropout_keep_prob)
     cells.append(cell)
 
-  cell = tf.contrib.rnn_cell.MultiRNNCell(cells, state_is_tuple=state_is_tuple)
+  cell = tf.contrib.rnn.rnn_cell.MultiRNNCell(cells, state_is_tuple=state_is_tuple)
   if hparams.attn_length:
     cell = tf.contrib.rnn.AttentionCellWrapper(
         cell, hparams.attn_length, state_is_tuple=state_is_tuple)
