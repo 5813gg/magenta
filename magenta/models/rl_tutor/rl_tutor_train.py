5b46531061030f2a3fee462aeb06925d88848a79
Natasha Jaques <jaquesn@mit.edu>
2017-02-22 13:13:20 -0500

    fix train code to train 512 lstm

--- magenta/models/rl_tutor/rl_tutor_train.py
+++ magenta/models/rl_tutor/rl_tutor_train.py
@@ -19,22 +19,22 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('domain_application', 'smiles',
                            'Application for which you are training model. Can either '
                            'be smiles (for molecule generation) or melody (for music).')
-tf.app.flags.DEFINE_string('output_dir', '/home/natasha/Dropbox/Google/SMILES-Project/output/round4/',
+tf.app.flags.DEFINE_string('output_dir', '/home/natasha/Dropbox/Google/SMILES-Project/output/prioritized_replay_512/',
                            'Directory where the model will save its'
                            'compositions and checkpoints (midi files)')
-tf.app.flags.DEFINE_string('rnn_checkpoint_dir', '/home/natasha/Dropbox/Google/SMILES-Project/models/',
+tf.app.flags.DEFINE_string('rnn_checkpoint_dir','/home/natasha/Dropbox/Google/SMILES-Project/models/smiles_rnn_512/',
                            'Path to directory holding checkpoints for pre-trained rnn'
                            'model. These will be loaded into the NoteRNNLoader class '
                            'object. The directory should contain a train subdirectory')
-tf.app.flags.DEFINE_string('rnn_checkpoint_name', 'smiles_rnn_100_350001.ckpt',
+tf.app.flags.DEFINE_string('rnn_checkpoint_name', 'smiles_rnn_31001',
                            'Filename of a checkpoint within the '
                            'rnn_checkpoint_dir directory.')
-tf.app.flags.DEFINE_float('reward_scaler', 0.25,
+tf.app.flags.DEFINE_float('reward_scaler', 0.4,
                           'The weight placed on music theory rewards')
 tf.app.flags.DEFINE_string('algorithm', 'psi',
                            'The name of the algorithm to use for training the'
                            'model. Can be q, psi, or g')
-tf.app.flags.DEFINE_integer('training_steps', 2000000,
+tf.app.flags.DEFINE_integer('training_steps', 1000000,
                             'The number of steps used to train the model')
 tf.app.flags.DEFINE_integer('exploration_steps', 0,
                             'The number of steps over which the models'
@@ -91,14 +91,26 @@ def main(_):
                           algorithm=FLAGS.algorithm)
   elif FLAGS.domain_application == 'smiles':
     import smiles_tutor
-    hparams = rl_tutor_ops.smiles_hparams()
+    #hparams = rl_tutor_ops.smiles_hparams()
+
+    hparams = rl_tutor_ops.HParams(use_dynamic_rnn=True,
+                                    batch_size=128,
+                                    lr=0.0002,
+                                    l2_reg=2.5e-5,
+                                    clip_norm=5,
+                                    initial_learning_rate=0.01,
+                                    decay_steps=1000,
+                                    decay_rate=0.85,
+                                    rnn_layer_sizes=[512],
+                                    one_hot_length=35,
+                                    exponentially_decay_learning_rate=True)
 
     rl_tutor_hparams = rl_tutor_ops.HParams(random_action_probability=0.01,
                                             store_every_nth=1,
-                                            train_every_nth=100,
+                                            train_every_nth=50,
                                             minibatch_size=512,
                                             discount_rate=0.95,
-                                            max_experience=350000,
+                                            max_experience=500000,
                                             target_network_update_rate=0.01,
                                             initial_learning_rate=0.0001)
 
