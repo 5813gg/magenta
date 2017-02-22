9155f5c0e9efb7520ef8761aea3bcb640cac32cf
Natasha Jaques <jaquesn@mit.edu>
2017-02-22 15:09:04 -0500

    temporarily changin tf.nn.rnn_cell to tf.contrib.rnn_cell

--- magenta/models/rl_tutor/rl_tutor_ops.py
+++ magenta/models/rl_tutor/rl_tutor_ops.py
@@ -340,20 +340,22 @@ def get_next_file_name(directory, prefix, extension):
   return name
 
 def make_cell(hparams, note_rnn_type, state_is_tuple=False):
-  """Makes a basic LSTM cell for use in the NoteRNNLoader graph."""
+  """Makes a basic LSTM cell for use in the NoteRNNLoader graph.
+  
+  TEMPORARILY CHANGING tf.nn.rnn_cell to tf.contrib.rnn_cell"""
   cells = []
   for num_units in hparams.rnn_layer_sizes:
     if note_rnn_type == 'default':
-      cell = tf.nn.rnn_cell.LSTMCell(
+      cell = tf.contrib.rnn_cell.LSTMCell(
           num_units, state_is_tuple=state_is_tuple)
     else:
-      cell = tf.nn.rnn_cell.BasicLSTMCell(
+      cell = tf.contrib.rnn_cell.BasicLSTMCell(
           num_units, state_is_tuple=state_is_tuple)
-      cell = tf.nn.rnn_cell.DropoutWrapper(
+      cell = tf.contrib.rnn_cell.DropoutWrapper(
           cell, output_keep_prob=hparams.dropout_keep_prob)
     cells.append(cell)
 
-  cell = tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=state_is_tuple)
+  cell = tf.contrib.rnn_cell.MultiRNNCell(cells, state_is_tuple=state_is_tuple)
   if hparams.attn_length:
     cell = tf.contrib.rnn.AttentionCellWrapper(
         cell, hparams.attn_length, state_is_tuple=state_is_tuple)
